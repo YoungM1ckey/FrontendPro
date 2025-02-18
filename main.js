235d8da6b55ec75cf1dd385f229ef0f1d5373128
Maksim <mickeyrossgame@gmail.com>
2025-02-18 21:20:55 +0200

    alert

---
+++ main.js
@@ -0,0 +1,5 @@
+let nameUser = prompt("What is your name?");
+let emailUser = prompt("What is your email address?");
+let ageUser = prompt("How old are you?");
+
+alert(`Your name is ${nameUser}, Your age is ${ageUser} and Your email ${emailUser}`);
