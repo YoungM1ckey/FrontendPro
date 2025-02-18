398a394641a14986fc75c74eb951a15a8426dc40
Maksim <mickeyrossgame@gmail.com>
2025-02-18 19:27:14 +0200

    data enumeration

---
+++ main.js
@@ -0,0 +1,12 @@
+const name = "Maksim";
+console.log(typeof name);
+const age = 22;
+console.log(typeof age);
+const boolean = false;
+console.log(typeof boolean);
+const bigNumber = 123314325239239432558n;
+console.log(typeof bigNumber);
+const none = null;
+console.log(typeof none);
+const undefinedType = undefined;
+console.log(typeof undefinedType);
