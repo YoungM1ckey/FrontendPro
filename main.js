f7eef7b5b67de524da271c9173dc1aafc473bb53
Maksim <mickeyrossgame@gmail.com>
2025-02-23 14:33:04 +0200

    first

---
+++ main.js
@@ -0,0 +1,6 @@
+let ageUser = prompt("How old are you?");
+let cityUser = prompt("We are you from?");
+let favoriteSport = prompt("What you favorite sport?");
+
+alert(`You are ${ageUser} years old /n 
+is ${cityUser} and ${favoriteSport}`);
