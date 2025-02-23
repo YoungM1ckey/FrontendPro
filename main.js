9de9f73b3ebe32d88d63bedf7969744f67da6c55
Maksim <mickeyrossgame@gmail.com>
2025-02-23 14:36:19 +0200

    first

--- main.js
+++
@@ -1,9 +0,0 @@
-let numberFromUser = prompt("Enter your number");
-
-let numberFirst = numberFromUser / 100 | 0;
-numberFromUser %= 100;
-let numberSecond = numberFromUser / 10 | 0;
-numberFromUser %= 10;
-let numberThird = numberFromUser / 1 | 0;
-numberFromUser %= 1;
-console.log(numberFirst, numberSecond, numberThird);
