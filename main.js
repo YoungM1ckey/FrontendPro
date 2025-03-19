f122e3320107d1a7d0c0d5c0d769288920d0fe5d
Maksim <mickeyrossgame@gmail.com>
2025-03-19 13:05:47 +0200

    )

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const buttonApplyColor = document.querySelector("#buttonApplyColor");
 const textForChangeColor = document.querySelector("#textForChangeColor");
+const colorPicker = document.querySelector("#colorPicker");
+
+let originalColor = window.getComputedStyle(textForChangeColor).color;
 
 buttonApplyColor.addEventListener("click", (e) => {
-    if (textForChangeColor.style.color === "purple") {
-        textForChangeColor.style.color = "black";
-        document.querySelector(".block").style.backgroundColor = "#fff";
+    const currentColor = window.getComputedStyle(textForChangeColor).color;
+
+    if (currentColor === originalColor) {
+        textForChangeColor.style.color = colorPicker.value;
     } else {
-        textForChangeColor.style.color = "purple";
-        document.querySelector(".block").style.backgroundColor = "#ccc";
+        textForChangeColor.style.color = originalColor;
     }
 })
