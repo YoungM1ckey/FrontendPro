46421ef2c05764d236a4557e883eecf94a3c74cb
Maksim <mickeyrossgame@gmail.com>
2025-03-22 13:03:23 +0200

    first

---
+++ main.js
@@ -0,0 +1,17 @@
+const nameCreateNewTask = document.querySelector("#nameCreateNewTask");
+const btnCreateNewTask = document.querySelector("#btnCreateNewTask");
+const tasks = document.querySelector("#tasks");
+
+tasks.addEventListener("click", (e) => {
+    e.target.parentElement.remove();
+})
+
+btnCreateNewTask.addEventListener("click", () => {
+    const name = nameCreateNewTask.value.trim();
+    if (nameCreateNewTask.value.trim()) {
+        const li = document.createElement("li");
+        li.innerHTML = `${name} <button type="button">Delete</button>`;
+        tasks.appendChild(li)
+    }
+    nameCreateNewTask.value = "";
+})
