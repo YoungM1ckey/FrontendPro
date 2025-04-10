5f91c346d92a467a271bbe4644aeed32e0feae34
Maksim <mickeyrossgame@gmail.com>
2025-04-10 10:30:24 +0300

    clear commit

--- main.js
+++ main.js
@@ -27,12 +27,12 @@ class BankAccount {
 
 const account1 = new BankAccount(1000);
 
-console.log(account1.getBalance()); // 1000
+console.log(account1.getBalance());
 
 account1.deposit(500);
 
-console.log(account1.getBalance()); // 1500
+console.log(account1.getBalance());
 
 account1.withdraw(200);
 
-console.log(account1.getBalance()); // 1300
+console.log(account1.getBalance());
