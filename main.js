58b09627bc3a5019e91f7f6ca613788adf06a77c
Maksim <mickeyrossgame@gmail.com>
2025-04-16 11:39:43 +0300

    first

---
+++ main.js
@@ -0,0 +1,17 @@
+let time = 15;
+const timerElement = document.querySelector('#timer');
+function displayTimer(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const second = seconds % 60;
+    return `${minutes.toString().padStart(2, '0')}:${second.toString().padStart(2, '0')}`;
+}
+timerElement.textContent = displayTimer(time);
+
+const timerInterval = setInterval(() => {
+    time--;
+    timerElement.textContent = displayTimer(time);
+    if (time <= 0) {
+        clearInterval(timerInterval);
+        timerElement.textContent = "00:00";
+    }
+}, 1000);
